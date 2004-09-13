4f26ea2ab88fb3ed0ffbd78fa882cb495bed87f0
tajbender <tajbender@users.noreply.github.com>
2004-09-13 19:49:21 +0000

    Added classes ExtMenuBarItem and ExtButtonItem, modified interface IGUIAction

--- src/Electrifier - Main/Core/Forms/IGUIAction.cs
+++ src/Electrifier - Main/Core/Forms/IGUIAction.cs
@@ -8,11 +8,14 @@
 using System;
 
 namespace Electrifier.Core.Forms {
+	/// <summary>
+	/// ExecutionEventArgs-class and ExecutionEventHandler delegate
+	/// </summary>
 	public class ExecutionEventArgs : EventArgs {
 		private object tagData = null;
 		public  object TagData { get { return tagData; } }
 
-		public ExecutionEventArgs(object tagData) {
+		public ExecutionEventArgs(object tagData) : base() {
 			this.tagData = tagData;
 		}
 	}
@@ -20,23 +23,36 @@ namespace Electrifier.Core.Forms {
 	public delegate void ExecutionEventHandler(object source, ExecutionEventArgs e);
 
 	/// <summary>
+	/// EnabledChangedEventArgs-class and EnabledChangedEventHandler delegate
+	/// </summary>
+	public class EnabledChangedEventArgs : EventArgs {
+		private bool enabled;
+		public  bool Enabled { get { return enabled; } }
+
+		public EnabledChangedEventArgs(bool enabled) : base() {
+			this.enabled = enabled;
+		}
+	}
+
+	public delegate void EnabledChangedEventHandler(object source, EnabledChangedEventArgs e);
+
+	/// <summary>
 	/// Zusammenfassung für IGUIAction.
 	/// </summary>
 	public interface IGUIAction {
 		// Properties
 		Guid Guid              { get; }
-		bool IsEnabled         { get; }
+		bool Enabled           { get; }
 //		bool IsSessionSpecific { get; }
 		int  ImageIndex        { get; }
 
 		// Methods
-		object CreateExecutedEventTagData(object sender);
-		void   Execute                   (object sender);
-		void   Enable                    (object sender);
-		void   Disable                   (object sender);
+		void Execute(object sender);
+		void Enable (object sender);
+		void Disable(object sender);
 
 		// Events
-		event ExecutionEventHandler Execution;
-		event EventHandler          EnabledChanged;
+		event ExecutionEventHandler      Execution;
+		event EnabledChangedEventHandler EnabledChanged;
 	}
 }
