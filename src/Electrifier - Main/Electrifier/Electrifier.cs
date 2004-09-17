b85521c9128621d9ba8666d4407291f75b751a30
tajbender <tajbender@users.noreply.github.com>
2004-09-17 16:36:35 +0000

    First changes caused by introducing an configuration file, added IPersistent-interface

--- src/Electrifier - Main/Electrifier/Electrifier.cs
+++ src/Electrifier - Main/Electrifier/Electrifier.cs
@@ -57,6 +57,11 @@ namespace Electrifier {
 			AppContext appContext = new AppContext(args, applicationIcon, splashScreen.SplashScreenBitmap, splashScreen);
 			Application.Run(appContext);
 
+			// Save configuration file
+			if(true == true) {	// TODO: uebergabeparameter zum ausschalten & exceptions checken
+				appContext.SaveConfiguration();
+			}
+
 			// Free used resources
 			applicationIcon.Dispose();
 			splashScreen.Dispose();
