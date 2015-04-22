976f0de365d6bb67a35a3bf1414da19388a28758
tajbender <tajbender@users.noreply.github.com>
2015-04-22 13:05:25 +0200

    - Added "-portable" and "-incognito" command line switches - Modified configuration file handling:   * default location changed to "C:\Users\*\AppData\Local\electrifier\electrifier.config.xml"   * Introduced "-portable"-command switch, see issue #5 "Add "-portable" command switch, storing configuration in application directory instead of "LocalApplicationData" #5" - Minor tweaks

--- src/Electrifier - Main/Core/AppContext.cs
+++ src/Electrifier - Main/Core/AppContext.cs
@@ -12,20 +12,33 @@ using Electrifier.Core.Shell32.Services;
 
 namespace Electrifier.Core {
 	/// <summary>
-	/// Zusammenfassung für AppContext.
+	/// AppContext acts as a singleton which instantiates all the basic services and gets the user interface started up
 	/// </summary>
 	public sealed class AppContext : ApplicationContext, IPersistentFormContainer {
 		// Static member variables and properties
-		private static AppContext instance       = null;
-		public  static AppContext Instance       { get { return instance; } }
-		private static Icon       icon           = null;
-		public  static Icon       Icon           { get { return icon; } }
-		private static Bitmap     logo           = null;
-		public  static Bitmap     Logo           { get { return logo; } }
-		private static ArrayList  openWindowList = new ArrayList();
-		public  static ArrayList  OpenWindowList { get { return openWindowList; } }
-		private static NotifyIcon notifyIcon     = new NotifyIcon();
-		public  static NotifyIcon NotifyIcon     { get { return notifyIcon; } }
+		private static AppContext instance = null;
+		public static AppContext Instance { get { return AppContext.instance; } }
+		private static Icon icon = null;
+		public static Icon Icon { get { return AppContext.icon; } }
+		private static Bitmap logo = null;
+		public static Bitmap Logo { get { return AppContext.logo; } }
+		private static ArrayList openWindowList = new ArrayList();
+		public static ArrayList OpenWindowList { get { return AppContext.openWindowList; } }
+		private static NotifyIcon notifyIcon = new NotifyIcon();
+		public static NotifyIcon NotifyIcon { get { return AppContext.notifyIcon; } }
+		private static bool isPortable = false;
+		public static bool IsPortable { get { return AppContext.isPortable; } }
+		private static bool isIncognito = false;
+		public static bool IsIncognito { get { return AppContext.isIncognito; } }
+		private static string configFileName = @"electrifier.config.xml";
+		private static string configFullPathFileName {
+			get {
+				if (AppContext.IsPortable)
+					return Path.Combine(Application.StartupPath, AppContext.configFileName);
+				else
+					return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"electrifier", AppContext.configFileName);
+			}
+		}
 
 		/// <summary>
 		/// The default constructor of the AppContext.
@@ -38,45 +51,44 @@ namespace Electrifier.Core {
 		/// <param name="appLogo">The logo resource used by this application</param>
 		/// <param name="splashScreenForm">The form representing the logo as splash screen</param>
 		public AppContext(string[] args, Icon appIcon, Bitmap appLogo, Form splashScreenForm) {
-			bool showMainWindow = true;
+			bool noMainWindow = false;
 
 			// Initialize application context
-			instance = RegisterAppContextInstance(this);
-			icon     = appIcon;
-			logo     = appLogo;
+			AppContext.instance = RegisterAppContextInstance(this);
+			AppContext.icon = appIcon;
+			AppContext.logo = appLogo;
 
 			// Initialize basic services
 			ServiceManager.Services.AddService(new DesktopFolderInstance());
 			ServiceManager.Services.AddService(new IconManager());
 			ServiceManager.Services.AddService(new PIDLManager());
 
-			// Search argument list for MainWindow-related arguments
-			foreach(string arg in args) {
-				if(arg.ToLower().Equals("/nomainwindow")) {
-					showMainWindow = false;
-				}
+			// Search argument list for AppContext-related arguments
+			foreach (string arg in args) {
+				// NoMainWindow: Don't show main window, just reside in notify tray
+				if (arg.ToLower().Equals("/nomainwindow"))
+					noMainWindow = true;
+
+				// Portable: Issue #5: Add "-portable" command switch, storing configuration in application directory instead of "LocalApplicationData"
+				if (arg.ToLower().Equals("/portable"))
+					AppContext.isPortable = true;
+
+				// Incognito: Don't modify configuration file on exit, thus don't make session parameters/changes persistent
+				if (arg.ToLower().Equals("/incognito"))
+					AppContext.isIncognito = true;
 			}
 
 			// Create NotifyIcon
 			notifyIcon.Icon = new Icon(appIcon, 16, 16);	// TODO: width & height from regisry?!?
 			notifyIcon.Visible = true;
 
-//						// Open new MainWindow, if not diabled
-//						if(showMainWindow) {
-//							MainWindowForm mainWindowForm = new MainWindowForm();
-//							mainWindowForm.Show();
-//			
-//							openWindowList.Add(mainWindowForm);
-//			
-//							// TODO: Action definieren, die neuen Electrifier aufmacht...
-//							// TODO: Config auslesen, wenn gewuenscht this.OnMainFormClosed checken und
-//							//       application am leben erhalten (Dialog "You closed all electrifier windows.
-//							//       do you want to stay electrifier active in tray" blablablubb)
-//							this.MainForm = mainWindowForm;
-//						}
+			// TODO: Action definieren, die neuen Electrifier aufmacht...
+			// TODO: Config auslesen, wenn gewuenscht this.OnMainFormClosed checken und
+			//       application am leben erhalten (Dialog "You closed all electrifier windows.
+			//       do you want to stay electrifier active in tray" blablablubb)
 
 			// Try to load and apply the configuration used for this session
-            if (!RestoreConfiguration()) {
+            if (RestoreConfiguration() == false) {
                 // Start a new session since restoring configuration failed
                 MainWindowForm mainWindowForm = new MainWindowForm();
                 mainWindowForm.Show();
@@ -85,7 +97,7 @@ namespace Electrifier.Core {
                 this.MainForm = mainWindowForm;
             }
 
-			if(showMainWindow) {
+			if(noMainWindow == false) {
 				// TODO: Wenn alle minimiert waren werden sie es auch diesmal sein!
 				Form form = openWindowList[0] as Form;
 
@@ -94,20 +106,17 @@ namespace Electrifier.Core {
 				MainForm = form;
 			}
 
-			// Add handler to save configuration before closing
+			// Add ThreadExit-handler to save configuration and dispose NotifyIcon when closing
 			ThreadExit += new EventHandler(AppContext_ThreadExit);
 
 			// Finally close splash screen
 			splashScreenForm.Close();
 		}
 
-		private void AppContext_ThreadExit(object sender, EventArgs e) 
-		{
+		private void AppContext_ThreadExit(object sender, EventArgs e) {
 			// Save configuration file
-			if(true == true) 
-			{	// TODO: uebergabeparameter zum ausschalten & exceptions checken
+			if (AppContext.IsIncognito == false)
 				SaveConfiguration();
-			}
 
 			// Destroy NotifyIcon
 			AppContext.NotifyIcon.Visible = false;
@@ -127,14 +136,14 @@ namespace Electrifier.Core {
 		}
 
 		public bool RestoreConfiguration() {
-			return RestoreConfiguration("Default.Config.xml");
+			return RestoreConfiguration(AppContext.configFullPathFileName);
 		}
 
-		public bool RestoreConfiguration(string configurationFileName) {
+		public bool RestoreConfiguration(string configFileName) {
 			try {
-                if (File.Exists(configurationFileName)) {
+                if (File.Exists(configFileName)) {
                     XmlDocument configXml = new XmlDocument();
-                    configXml.Load(configurationFileName);
+                    configXml.Load(configFileName);
 
                     XmlNode node = configXml.DocumentElement;
                     string text = node.LocalName;
@@ -145,49 +154,61 @@ namespace Electrifier.Core {
 			} catch(Exception e) {
 				// TODO: Add error-handling here...
 
-                System.Windows.Forms.MessageBox.Show("AppContext.RestoreConfiguration: Exception occured:" + e.Message, "electrifier: Error!");
+				MessageBox.Show("AppContext.RestoreConfiguration: Error while restoring configuration file: \n\n" + e.Message,
+					"electrifier: We're sorry, but a runtime error occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
 			}
 
             return false;
 		}
 
 		public void SaveConfiguration() {
-			SaveConfiguration("Default.Config.xml");
+			SaveConfiguration(AppContext.configFullPathFileName);
 		}
 
-		public void SaveConfiguration(string fileName) {
-			// Create XmlDocument to store all the configuration parameters
-			XmlDocument configXml = new XmlDocument();
-			configXml.AppendChild(configXml.CreateComment("\n\n" +
-				"Electrifier configuration file\n\n" +
-				"This is a machine generated file, created by Electrifier application\n" +
-				"For more information about electrifier visit http://www.electrifier.org\n\n" +
-				"DO NOT EDIT THIS FILE MANUALLY, CHANGES MAY CAUSE THE\n" +
-				"CONTAINED CONFIGURATION INFORMATION BECOME CORRUPTED!\n\n" +
-				"Copyright (c) 2015 by electrifier.org and its owners\n\n"));
+		public void SaveConfiguration(string configFullPathFileName) {
+			try {
+				// Create XmlDocument to store all the configuration parameters
+				XmlDocument configXml = new XmlDocument();
+				configXml.AppendChild(configXml.CreateComment("\n\n" +
+					"Electrifier configuration file\n\n" +
+					"This is a machine generated file, created by Electrifier application\n" +
+					"For more information about electrifier visit http://www.electrifier.org\n\n" +
+					"DO NOT EDIT THIS FILE MANUALLY, CHANGES MAY CAUSE THE\n" +
+					"CONTAINED CONFIGURATION INFORMATION BECOME CORRUPTED!\n\n" +
+					"Copyright (c) 2015 by electrifier.org and its owners\n\n"));
 
-			// Create the root xml node
-			XmlNode      configNode   = configXml.CreateElement("Electrifier");
-			XmlAttribute fileTypeAttr = configXml.CreateAttribute("ContentDescription");
-			fileTypeAttr.Value        = "Configuration";
-			XmlAttribute versionAttr  = configXml.CreateAttribute("FileVersion");
-			versionAttr.Value         = "0.1";
-			XmlAttribute creationAttr = configXml.CreateAttribute("CreationDateTime");
-			creationAttr.Value        = String.Format("{0:s}", DateTime.UtcNow);
-			configNode.Attributes.Append(fileTypeAttr);
-			configNode.Attributes.Append(versionAttr);
-			configNode.Attributes.Append(creationAttr);
+				// Create the root xml node
+				XmlNode configNode = configXml.CreateElement("Electrifier");
+				XmlAttribute fileTypeAttr = configXml.CreateAttribute("ContentDescription");
+				fileTypeAttr.Value = "Configuration";
+				XmlAttribute versionAttr = configXml.CreateAttribute("FileVersion");
+				versionAttr.Value = "0.1";
+				XmlAttribute creationAttr = configXml.CreateAttribute("CreationDateTime");
+				creationAttr.Value = String.Format("{0:s}", DateTime.UtcNow);
+				configNode.Attributes.Append(fileTypeAttr);
+				configNode.Attributes.Append(versionAttr);
+				configNode.Attributes.Append(creationAttr);
 
-			// Append all xml node elements describing the actual configuration
-			configNode.AppendChild(CreatePersistenceInfo(configXml));
-			configXml.AppendChild(configNode);
+				// Append all xml node elements describing the actual configuration
+				configNode.AppendChild(CreatePersistenceInfo(configXml));
+				configXml.AppendChild(configNode);
 
-			// Write the XmlDocument to file
-			fileName                = Application.StartupPath + "\\" + fileName;
-			XmlTextWriter xmlWriter = new System.Xml.XmlTextWriter(fileName, new UnicodeEncoding());
-			xmlWriter.Formatting    = Formatting.Indented;
+				// Ensure the directory exists
+				FileInfo configFullPathFileInfo = new FileInfo(configFullPathFileName);
 
-			configXml.Save(xmlWriter);
+				if(!configFullPathFileInfo.Directory.Exists)
+					configFullPathFileInfo.Directory.Create();
+
+				// Write the XmlDocument to file
+				XmlTextWriter xmlWriter = new System.Xml.XmlTextWriter(configFullPathFileName, new UnicodeEncoding());
+				xmlWriter.Formatting = Formatting.Indented;
+
+				configXml.Save(xmlWriter);
+			}
+			catch (Exception e) {
+				MessageBox.Show("AppContext.SaveConfiguration: Error while writing configuration file: \n\n" + e.Message,
+					"electrifier: We're sorry, but a runtime error occurred!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
 		}
 
 		/// <summary>
