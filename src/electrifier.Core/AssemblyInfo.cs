ca24ac730e6a458e298ce62b89a0196f43a9e865
tajbender <tajbender@electrifier.org>
2017-08-16 23:27:56 +0200

    Do Refactoring: Add LICENSE, apply license to all source code files

--- src/electrifier.Core/AssemblyInfo.cs
+++ src/electrifier.Core/AssemblyInfo.cs
@@ -1,15 +1,28 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+/*
+** 
+**  electrifier
+** 
+**  Copyright 2017 Thorsten Jung, www.electrifier.org
+**  
+**  Licensed under the Apache License, Version 2.0 (the "License");
+**  you may not use this file except in compliance with the License.
+**  You may obtain a copy of the License at
+**  
+**      http://www.apache.org/licenses/LICENSE-2.0
+**  
+**  Unless required by applicable law or agreed to in writing, software
+**  distributed under the License is distributed on an "AS IS" BASIS,
+**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**  See the License for the specific language governing permissions and
+**  limitations under the License.
+**
+*/
 
-///
-// electrifier.Core.dll - AssemblyInfo.cs
-///
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("electrifier.Core.dll - Core services")]
-[assembly: AssemblyDescription("Provides electrifier core services")]
-//[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("electrifier.Core.dll")]
 [assembly: Guid("8F2A9E0F-47E0-45D7-9A86-3A58332D4990")]
--- src/electrifier/CommonAssemblyInfo.cs
+++ src/electrifier/CommonAssemblyInfo.cs
@@ -1,19 +1,36 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿/*
+** 
+**  electrifier
+** 
+**  Copyright 2017 Thorsten Jung, www.electrifier.org
+**  
+**  Licensed under the Apache License, Version 2.0 (the "License");
+**  you may not use this file except in compliance with the License.
+**  You may obtain a copy of the License at
+**  
+**      http://www.apache.org/licenses/LICENSE-2.0
+**  
+**  Unless required by applicable law or agreed to in writing, software
+**  distributed under the License is distributed on an "AS IS" BASIS,
+**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**  See the License for the specific language governing permissions and
+**  limitations under the License.
+**
+*/
+
+using System.Reflection;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyProduct("electrifier Desktop Enhancement Suite")]
+[assembly: AssemblyProduct("electrifier")]
 [assembly: AssemblyCompany("electrifier.org")]
-[assembly: AssemblyCopyright("Copyright © 2016 electrifier.org, Thorsten Jung")]
-//[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright 2017 Thorsten Jung, www.electrifier.org")]
 
 #if DEBUG
-	[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyConfiguration("Debug")]
 #else
-	[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyConfiguration("Release")]
 #endif
 
 // Version information for an assembly consists of the following four values:
@@ -26,5 +43,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.17.8")]
+[assembly: AssemblyFileVersion("0.1.17.8")]
