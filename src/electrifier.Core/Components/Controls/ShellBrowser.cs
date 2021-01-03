ae43bf61842da5f756b3c267c06d8da3edbf2a2e
tajbender <tajbender@users.noreply.github.com>
2021-01-03 11:31:21 +0100

    Add MessageSFVCB-Implementation with its improvements: - In RecreateShellView(), catch {"The operation was canceled by the user. (Exception from HRESULT: 0x800704C7)"} - In MessageSFVCB, catch SFVM_SELECTIONCHANGED and SFVM_LISTREFRESHED events - Add many informational links and comments

--- src/electrifier.Core/Components/Controls/ShellBrowser.cs
+++ src/electrifier.Core/Components/Controls/ShellBrowser.cs
@@ -31,6 +31,11 @@ using Vanara.Windows.Shell;
 
 // http://www.sky.franken.de/doxy/explorer/structIShellBrowserImpl.html
 
+/// <summary>
+/// For extened undocumented ListView Information, see this: https://www.codeproject.com/Articles/35197/Undocumented-List-View-Features
+/// https://stackoverflow.com/questions/15750842/allow-selection-in-explorer-style-list-view-to-start-in-the-first-column
+/// 
+/// </summary>
 
 
 namespace electrifier.Core.Components.Controls
@@ -97,14 +102,20 @@ namespace electrifier.Core.Components.Controls
     //
     // NOTE: For Keyboard handling, have a look here: https://docs.microsoft.com/en-us/windows/win32/api/oleidl/nn-oleidl-ioleinplaceactiveobject
     //
-    // TODO: Creating ViewWindow using '.CreateViewWindow(' fails on Zip-Folders; I barely remember we have to react on DDE_ - Messages for this to work?!? -> When Implementing ICommonDlgBrowser, this works! => Fixed by removing IShellFolderViewCB
+    // TODO: Creating ViewWindow using '.CreateViewWindow(' fails on Zip-Folders; I barely remember we have to react on DDE_ - Messages for this to work?!? -> When Implementing ICommonDlgBrowser, this works!
+    //       => Fixed by removing IShellFolderViewCB ==> Fixed again by returning HRESULT.E_NOTIMPL from MessageSFVCB
 
     /// <summary>
-    /// 
-    /// This implements the following Interfaces:
-    /// ICommonDlgBrowser: To be able to browse into Zip-Folders => NO! // NOTE: 01/01/21: IShellFolderViewCB => If you provide this, navigating into zipped Folders won't work any more. Maybe you have to respond to one if it's Messages correctly.
-    /// 
-    /// https://www.codeproject.com/Articles/28961/Full-implementation-of-IShellBrowser
+    /// Encapsulates a <see cref="Shell32.IShellBrowser"/>-Implementation within an <see cref="UserControl"/>.<br/>
+    /// <br/>
+    /// Implements the following Interfaces:<br/>
+    /// - <seealso cref="IWin32Window"/><br/>
+    /// - <seealso cref="Shell32.IShellBrowser"/><br/>
+    /// - <seealso cref="Shell32.IShellFolderViewCB"/><br/>
+    /// - <seealso cref="Shell32.IServiceProvider"/><br/>
+    /// <br/>
+    /// For more Information on used techniques, see:<br/>
+    /// - <seealso href="https://www.codeproject.com/Articles/28961/Full-implementation-of-IShellBrowser"/><br/>
     /// 
     /// 
     /// Known Issues:
@@ -116,6 +127,9 @@ namespace electrifier.Core.Components.Controls
     /// - Keyboard input doesn't work so far.
     /// - DONE: Only Details-Mode should have column headers: (Using Shell32.FOLDERFLAGS.FWF_NOHEADERINALLVIEWS)
     ///   https://stackoverflow.com/questions/11776266/ishellview-columnheaders-not-hidden-if-autoview-does-not-choose-details
+    /// - TODO: CustomDraw, when currently no shellView available, draw white background to avoid flicker while navigating?!? => OR: Release shellViewWindow AFTER creating the new window...
+    /// - DONE: Network folder: E_FAIL => DONE: Returning HRESULT.E_NOTIMPL from MessageSFVCB fixes this 
+    /// - DONE: Disk Drive (empty): E_CANCELLED_BY_USER
     /// </summary>
     [ComVisible(true)]
     [ClassInterface(ClassInterfaceType.None)]
@@ -123,20 +137,29 @@ namespace electrifier.Core.Components.Controls
         : UserControl
         , IWin32Window      // See also: NativeWindow
         , Shell32.IShellBrowser
-        , Shell32.IServiceProvider
         , Shell32.IShellFolderViewCB
+        , Shell32.IServiceProvider
     {
 
-        //private Guid IID_IShellBrowser = new Guid("000214E2-0000-0000-C000-000000000046");
+        // OBSOLETE: private Guid IID_IShellBrowser = new Guid("000214E2-0000-0000-C000-000000000046");
 
-        //private const int WM_GETISHELLBROWSER = WM_USER + 0x00000007;
-        //private const int WM_USER = 0x00000400;          // See KB 157247
-
-
+        // OBSOLETE: private const int WM_GETISHELLBROWSER = WM_USER + 0x00000007;
+        // OBSOLETE: private const int WM_USER = 0x00000400;          // See KB 157247
 
         private ShellFolder currentFolder;
         private Shell32.IShellView shellView;
-        private HWND shellViewHandle;
+        private HWND shellViewWindow;         // Vanara -> IShellHandle ?
+        private Shell32.IFolderView2 folderView2;
+
+        #region Properties =====================================================================================================
+
+        private string emptyFolderText = "This folder\nis empty.";
+        [Category("Appearance"), Description("The default text that is displayed when an empty folder is shown.")]
+        public string EmptyFolderText
+        {
+            get => this.emptyFolderText;
+            set => this.SetEmptyFolderText(value);
+        }
 
         public ShellFolder CurrentFolder
         {
@@ -144,6 +167,14 @@ namespace electrifier.Core.Components.Controls
             set => this.SetCurrentFolder(value);
         }
 
+        #endregion ============================================================================================================
+
+        #region Published Events ==============================================================================================
+
+        [Category("Shell Event"), Description("ShellBowser has navigated to a new folder.")]
+        public event EventHandler<ShellBrowserNavigationCompleteEventArgs> NavigationComplete;
+
+        #endregion ============================================================================================================
 
 
         public ShellBrowser()
@@ -156,9 +187,9 @@ namespace electrifier.Core.Components.Controls
 
         private void ShellBrowser_Resize(object sender, EventArgs e)
         {
-            if (this.shellView != null && this.shellViewHandle != null)
+            if (this.shellView != null && this.shellViewWindow != null)
             {
-                User32.MoveWindow(this.shellViewHandle, 0, 0, this.ClientRectangle.Width, this.ClientRectangle.Height, false);
+                User32.MoveWindow(this.shellViewWindow, 0, 0, this.ClientRectangle.Width, this.ClientRectangle.Height, false);
             }
         }
 
@@ -185,15 +216,11 @@ namespace electrifier.Core.Components.Controls
 
             this.BrowseObject((IntPtr)this.CurrentFolder.PIDL, Shell32.SBSP.SBSP_ABSOLUTE);
         }
-
-
-        #region Published Events ==============================================================================================
-
-
-        [Category("Shell Event"), Description("ShellBowser has navigated to a new folder.")]
-        public event EventHandler<ShellBrowserNavigationCompleteEventArgs> NavigationComplete;
-
-        #endregion ============================================================================================================
+        protected virtual void SetEmptyFolderText(string value)
+        {
+            this.emptyFolderText = value;
+            this.folderView2?.SetText(Shell32.FVTEXTTYPE.FVST_EMPTYTEXT, value);
+        }
 
         protected void OnNavigationComplete(ShellFolder shellFolder)
         {
@@ -205,12 +232,21 @@ namespace electrifier.Core.Components.Controls
             }
         }
 
+
+        // We should use the following pattern:
+        // 1) Try to Create new ShellFolder, IFolderView2 and new ShellViewWindow
+        // 2) Raise event that navigation should be done, event-variable: "Target is valid" and "Allow Navigation"
+        // 3) When the owner responds with "OKAY", then navigate to the new folder, releasing the old ones.
+        //    If, however, the target is invalid, enter owner-drawn error mode.
+        //    In Erromode, show an error-glyph and an error-text. Use custom-drawn error-mode for designer also.
+        // 4) If not, Dispose the handles again.
+
         protected ShellFolder RecreateShellView(Shell32.PIDL pidl)
         {
             var shellFolder = new ShellFolder(pidl);
             var sfvCreate = new Shell32.SFV_CREATE()
             {
-                psfvcb = null, // NOTE: 01/01/21: IShellFolderViewCB => If you provide this, navigating into virtual zipped Folders won't work any more. Maybe you have to respond to one if it's Messages correctly.
+                psfvcb = this, // NOTE: 01/01/21: IShellFolderViewCB => If you provide this, navigating into virtual zipped Folders won't work any more. Maybe you have to respond to one if it's Messages correctly.
                 pshf = shellFolder.IShellFolder,
                 psvOuter = null,
             };
@@ -224,20 +260,22 @@ namespace electrifier.Core.Components.Controls
 
             /// TODO: Take care of threading here... The following has to be Invoked into the main thread.
 
-            if (!this.shellViewHandle.IsNull)
-                User32.DestroyWindow(this.shellViewHandle);
+            if (!this.shellViewWindow.IsNull)
+                User32.DestroyWindow(this.shellViewWindow);
 
             if (this.shellView != null)
             {
                 this.shellView.UIActivate(Shell32.SVUIA.SVUIA_DEACTIVATE);
 
+                // Use COMReleaser: https://github.com/dahall/Vanara/blob/master/Core/InteropServices/ComReleaser.cs
                 if (this.shellView.GetType().IsCOMObject)
                     Marshal.ReleaseComObject(this.shellView);
             }
 
-            Shell32.IFolderView2 folderview = (Shell32.IFolderView2)newShellView;
-            if (folderview is null)
+            this.folderView2 = (Shell32.IFolderView2)newShellView;
+            if (this.folderView2 is null)
                 MessageBox.Show("No IFolderView2");
+            this.SetEmptyFolderText(this.emptyFolderText);
             //Shell32.IShellView3 shellView3 = (Shell32.IShellView3)newShellView;
             //if(shellView3 is null)
             //    MessageBox.Show("No ShellView3");
@@ -245,7 +283,7 @@ namespace electrifier.Core.Components.Controls
 
 
 
-            folderview.SetText(Shell32.FVTEXTTYPE.FVST_EMPTYTEXT, "This folder\nis empty!");        // TODO: Put into property
+            this.folderView2.SetText(Shell32.FVTEXTTYPE.FVST_EMPTYTEXT, "This folder\nis empty.");
                                                                                                     //folderview.SetViewModeAndIconSize
 
             
@@ -267,7 +305,7 @@ namespace electrifier.Core.Components.Controls
 
 
 
-            var test = folderview.GetCurrentViewMode();
+            var test = this.folderView2.GetCurrentViewMode();
             AppContext.TraceDebug($"ViewMode: {test}");
 
             // internal IFolderView2 GetFolderView2() { try { return explorerBrowserControl?.GetCurrentView<IFolderView2>(); } catch { return null; } }
@@ -278,7 +316,18 @@ namespace electrifier.Core.Components.Controls
 
                 // TODO: For folder "Network", we'll get an COMException here: 0x80004005,      => // NOTE: 01/01/21: since dropping ICommonDlgBrowser Interface, there's no exception anymore.
                 // TODO: Zipped folders won't work at all :(                                    => // NOTE: 01/01/21: Since dropping IShellFolderViewCB, they work again
-                this.shellViewHandle = newShellView.CreateViewWindow(psvPrevious: null, pfs: folderSettings, psb: this, prcView: this.ClientRectangle);
+                try
+                {
+                    this.shellViewWindow = newShellView.CreateViewWindow(psvPrevious: null, pfs: folderSettings, psb: this, prcView: this.ClientRectangle);
+                }
+                catch (COMException ex)
+                {
+                    if (ex.HResult.Equals(-2147023673))       // {"The operation was canceled by the user. (Exception from HRESULT: 0x800704C7)"}
+                    {
+                        this.folderView2.SetText(Shell32.FVTEXTTYPE.FVST_EMPTYTEXT, "Cancelled by the user.");
+                    }
+                    else throw;
+                }
 
                 // TODO: Take care of disk drives with removeable media; => "Cancalled by the user" COMException
 
@@ -462,12 +511,144 @@ namespace electrifier.Core.Components.Controls
 
         #endregion ============================================================================================================
 
+        #region IShellFolderViewCB interface ==================================================================================
+
+        /// <summary>
+        /// Message hook of Interface <seealso href="Shell32.IShellFolderViewCB"/>.
+        /// <br/>
+        /// 
+        /// 
+        /// </summary>
+        /// <param name="uMsg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <param name="plResult"></param>
+        /// <returns></returns>
+        public HRESULT MessageSFVCB(Shell32.SFVM uMsg, IntPtr wParam, IntPtr lParam, ref IntPtr plResult)
+        {
+            //const OSFeature
+            const uint SFVM_SELECTIONCHANGED = 8;
+            const uint SFVM_LISTREFRESHED = 17;
+
+
+            /*
+             * Undocumented Messages:
+             * https://github.com/google/google-drive-shell-extension/blob/master/DriveFusion/ShellFolderViewCBHandler.cpp
+             * https://doxygen.reactos.org/d2/dbb/IShellFolderViewCB_8cpp.html and https://doxygen.reactos.org/d2/dbb/IShellFolderViewCB_8cpp_source.html
+             * 
+             * 
+             * Don't know if this is linked: https://docs.microsoft.com/en-us/windows/win32/shell/shellfolderviewoc-object
+
+            */
+
+            switch (uMsg)
+            {
+                // https://docs.microsoft.com/en-us/windows/win32/shell/sfvm-getnotify
+                // wParam = PIDL, lParam = lEvents
+                case Shell32.SFVM.SFVM_GETNOTIFY:
+                    //AppContext.TraceDebug($"SFVM_GETNOTIFY: {wParam} - {lParam}");
+
+                    //var test = (int)Shell32.SHCNE.SHCNE_UPDATEDIR + (int)Shell32.SHCNE.SHCNE_UPDATEITEM;
+                    //lParam = IntPtr.Zero;
+                    // See SFVM_GETNOTIFY: https://docs.microsoft.com/en-us/windows/win32/shell/sfvm-getnotify
+                    // Here we have to pass a combination of events we want to get notified of.
+                    break;
+
+                //case Shell32.SFVM.SFVM_UPDATESTATUSBAR:
+                //    AppContext.TraceDebug("SFVM_UPDATESTATUSBAR");
+                //    break;
+
+                //case (Shell32.SFVM)SFVM_ENUMERATEDITEMS:
+                //    AppContext.TraceDebug("SFVM_ENUMERATEDITEMS");
+                //    break;
+
+                case (Shell32.SFVM)SFVM_SELECTIONCHANGED:
+                    AppContext.TraceDebug("SFVM_SELECTIONCHANGED");     // wParam = 0x7200;
+                    return HRESULT.S_OK;
+
+                case (Shell32.SFVM)SFVM_LISTREFRESHED:
+                    AppContext.TraceDebug("SFVM_LISTREFRESHED");
+                    return HRESULT.S_OK;
+
+
+                //case Shell32.SFVM.SFVM_FSNOTIFY:
+                //    var msg = (Shell32.SHCNE)lParam; //
+                //    switch (msg)
+                //    {
+                //        case Shell32.SHCNE.SHCNE_UPDATEITEM: //
+                //            AppContext.TraceDebug("Update Item");
+
+                //            //if (wParam != IntPtr.Zero)
+                //            //{
+                //            //    var pidl = new Shell32.PIDL(wParam);
+                //            //    var item = new ShellItem(pidl);
+                //            //}
+
+                //            break;
+                //        case Shell32.SHCNE.SHCNE_ATTRIBUTES: // 0x2048
+
+                //            AppContext.TraceDebug("Attributes");
+
+
+
+                //            //var pidltest = new Shell32.PIDL(wParam);
+                //            //var testtext = pidltest.ToString();
+                //            //AppContext.TraceDebug($"UpdateItem: {testtext}");
+
+                //            break;
+                //        default:
+                //            break;
+
+                //    }
+
+
+                //var test2 = Shell32.SFVM;  SFVM_FSNOTIFY
+
+
+                //switch (lParam)
+
+
+                //var pidl = wParam;
+                //var levent = lParam;
+
+                //var test2 = Shell32.SHCNE.SHCNE_ATTRIBUTES;
+                //var pidltest = new Shell32.PIDL(wParam);
+
+
+                //AppContext.TraceDebug($"Notify: {levent} for pidl { pidl }");
+                //break;
+
+
+                default:
+                    //AppContext.TraceDebug($"MessageSVFCB: {uMsg}");
+                    break;
+            }
+            return HRESULT.E_NOTIMPL;  //HRESULT.E_NOTIMPL;
+        }
+
+        #endregion ============================================================================================================
 
         #region IServiceProvider interface ====================================================================================
 
+        /// <summary>
+        /// <see cref="Shell32.IServiceProvider"/>-Interface Implementation for <see cref="ShellBrowser"/>.<br/>
+        /// <br/>
+        /// Responds to the following Interfaces:<br/>
+        /// - <see cref="Shell32.IShellBrowser"/><br/>
+        /// - <see cref="Shell32.IShellFolderViewCB"/><br/>
+        /// </summary>
+        /// <param name="guidService">The service's unique identifier (SID).</param>
+        /// <param name="riid">The IID of the desired service interface.</param>
+        /// <param name="ppvObject">When this method returns, contains the interface pointer requested riid. If successful,
+        /// the calling application is responsible for calling IUnknown::Release using this value when the service is no
+        /// longer needed. In the case of failure, this value is NULL.</param>
+        /// <returns>
+        ///  <see cref="HRESULT.S_OK"/> or<br/>
+        ///  <see cref="HRESULT.E_NOINTERFACE"/>
+        /// </returns>
         public HRESULT QueryService(in Guid guidService, in Guid riid, out IntPtr ppvObject)
         {
-            // Guid("000214E2-0000-0000-C000-000000000046")
+            // IShellBrowser: Guid("000214E2-0000-0000-C000-000000000046")
             if (riid.Equals(typeof(Shell32.IShellBrowser).GUID))
             {
                 ppvObject = Marshal.GetComInterfaceForObject(this, typeof(Shell32.IShellBrowser));
@@ -475,7 +656,7 @@ namespace electrifier.Core.Components.Controls
                 return HRESULT.S_OK;
             }
 
-            // Guid("2047E320-F2A9-11CE-AE65-08002B2E1262")
+            // IShellFolderViewCB: Guid("2047E320-F2A9-11CE-AE65-08002B2E1262")
             if (riid.Equals(typeof(Shell32.IShellFolderViewCB).GUID))
             {
                 ppvObject = Marshal.GetComInterfaceForObject(this, typeof(Shell32.IShellFolderViewCB));
@@ -488,27 +669,6 @@ namespace electrifier.Core.Components.Controls
         }
 
         #endregion ============================================================================================================
-
-        #region IShellFolderViewCB interface ==================================================================================
-
-        public HRESULT MessageSFVCB(Shell32.SFVM uMsg, IntPtr wParam, IntPtr lParam, ref IntPtr plResult)
-        {
-            //AppContext.TraceDebug($"{uMsg}");
-            //switch (uMsg)
-            //{
-            //    case Shell32.SFVM.SFVM_UPDATESTATUSBAR:
-            //        AppContext.TraceDebug("SFVM_UPDATESTATUSBAR");
-            //        break;
-
-            //    default:
-            //        break;
-            //}
-            return HRESULT.S_OK;
-        }
-
-        #endregion ============================================================================================================
-
-
 
         #region Component Designer Support ====================================================================================
 
