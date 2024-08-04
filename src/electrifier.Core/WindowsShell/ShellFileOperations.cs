1b39615a2fd7238f88aca8f6632c1d392848c6aa
Thorsten Jung <tajbender@users.noreply.github.com>
2024-08-04 16:54:35 +0200

    Refactor

--- src/electrifier.Core/WindowsShell/ShellFileOperations.cs
+++ src/electrifier.Core/WindowsShell/ShellFileOperations.cs
@@ -1,45 +1,23 @@
-﻿/*
-** 
-**  electrifier
-** 
-**  Copyright 2017-19 Thorsten Jung, www.electrifier.org
-**  
-**  Licensed under the Apache License, Version 2.0 (the "License");
-**  you may not use this file except in compliance with the License.
-**  You may obtain a copy of the License at
-**  
-**      http://www.apache.org/licenses/LICENSE-2.0
-**  
-**  Unless required by applicable law or agreed to in writing, software
-**  distributed under the License is distributed on an "AS IS" BASIS,
-**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-**  See the License for the specific language governing permissions and
-**  limitations under the License.
-**
-*/
-
-using System;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Windows.Forms;
-
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using Vanara.PInvoke;
 using Vanara.Windows.Shell;
 
 namespace electrifier.Core.WindowsShell
 {
     /// <summary>
-    /// TODO: Remove <see cref="ShellFileOperations"/>, cause the multi-threaded.
+    /// TODO: Remove <see cref="ShellFileOperations"/>, cause multi-threaded Issues.
     /// Use <see cref="Vanara.Windows.Forms.ShellProgressDialog"/> instead.
     /// </summary>
     internal class ElShellFileOperations : IDisposable
     {
         public ShellFileOperations ShellFileOperations { get; }
-
         public bool ThrowUserCancellation { get; set; }
-
-        //public IEnumerable<ShellItem> SourceItems { get; }
-
-        //public IEnumerable<ShellItem> TargetItems { get; }
+        public ShellItemArray SourceItems { get; }
+        public ShellItemArray TargetItems { get; }
 
         public ElShellFileOperations(IWin32Window owner, ShellFileOperations.OperationFlags operationFlags)
         {
