f884b4686a0df8b6144930ca393f475914ce3e4b
Thorsten Jung <tajbender@users.noreply.github.com>
2024-08-05 11:02:37 +0200

    Cleanup Solution, Remove `Nlog`

--- src/electrifier.Core/LogContext.cs
+++ src/electrifier.Core/LogContext.cs
@@ -1,33 +1,10 @@
-﻿/*
-** 
-**  electrifier
-** 
-**  Copyright 2017-19 Thorsten Jung, www.electrifier.org
-**  
-**  Licensed under the Apache License, Version 2.0 (the "License");
-**  you may not use this file except in compliance with the License.
-**  You may obtain a copy of the License at
-**  
-**      http://www.apache.org/licenses/LICENSE-2.0
-**  
-**  Unless required by applicable law or agreed to in writing, software
-**  distributed under the License is distributed on an "AS IS" BASIS,
-**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-**  See the License for the specific language governing permissions and
-**  limitations under the License.
-**
-*/
-
-using NLog;
-using NLog.Config;
-using NLog.Targets;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace electrifier.Core
 {
@@ -37,7 +14,7 @@ namespace electrifier.Core
     /// </summary>
     public static class LogContext
     {
-        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+        private readonly Logger<string> = new ();
 
         public static void Initialize(string baseDirectory = null)
         {
@@ -56,14 +33,6 @@ namespace electrifier.Core
                 CreateDirs = true,
                 AutoFlush = true,
             };
-
-            logConfig.AddRuleForAllLevels(fileTarget);
-
-            NLog.LogManager.Configuration = logConfig;
-
-            Logger.Info("");
-            Logger.Info($"Logging into file 'logfile.txt' in folder '{baseDirectory}'");
-            Logger.Info("");
         }
 
         //[Conditional("DEBUG")]
